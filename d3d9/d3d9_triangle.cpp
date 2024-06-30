4cf5237c6255fce926a6e976d4e1fdcfd3ed18ae
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-07-01 00:07:00 +0300

    [d3d9] Expand d3d9-triangle listings

--- d3d9/d3d9_triangle.cpp
+++ d3d9/d3d9_triangle.cpp
@@ -79,7 +79,7 @@ class RGBTriangle {
             m_pp.hDeviceWindow = m_hWnd;
             // set to D3DSWAPEFFECT_COPY or D3DSWAPEFFECT_FLIP for no VSync
             m_pp.SwapEffect = D3DSWAPEFFECT_COPY;
-            // according to D3D8 spec "0 is treated as 1" here
+            // according to D3D9 spec "0 is treated as 1" here
             m_pp.BackBufferCount = 0;
             m_pp.BackBufferWidth = WINDOW_WIDTH;
             m_pp.BackBufferHeight = WINDOW_HEIGHT;
@@ -124,6 +124,287 @@ class RGBTriangle {
             }
 
             std::cout << format("Listed a total of ", totalAdapterModeCount, " adapter display modes") << std::endl;
+        }
+
+        // D3D Device back buffer formats check
+        void listBackBufferFormats(BOOL windowed) {
+            resetOrRecreateDevice();
+
+            HRESULT status;
+            D3DPRESENT_PARAMETERS bbPP;
+
+            memcpy(&bbPP, &m_pp, sizeof(m_pp));
+
+            // these are all the possible backbuffer formats, at least in theory
+            std::map<D3DFORMAT, char const*> bbFormats = { {D3DFMT_A8R8G8B8, "D3DFMT_A8R8G8B8"},
+                                                           {D3DFMT_X8R8G8B8, "D3DFMT_X8R8G8B8"},
+                                                           {D3DFMT_A1R5G5B5, "D3DFMT_A1R5G5B5"},
+                                                           {D3DFMT_X1R5G5B5, "D3DFMT_X1R5G5B5"},
+                                                           {D3DFMT_R5G6B5, "D3DFMT_R5G6B5"},
+                                                           {D3DFMT_A2R10G10B10, "D3DFMT_A2R10G10B10"} };
+
+            std::map<D3DFORMAT, char const*>::iterator bbFormatIter;
+
+            std::cout << std::endl;
+            if (windowed)
+                std::cout << "Device back buffer format support (windowed):" << std::endl;
+            else
+                std::cout << "Device back buffer format support (full screen):" << std::endl;
+
+            for (bbFormatIter = bbFormats.begin(); bbFormatIter != bbFormats.end(); bbFormatIter++) {
+                status = m_d3d->CheckDeviceType(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
+                                                bbFormatIter->first, bbFormatIter->first,
+                                                windowed);
+                if (FAILED(status)) {
+                    std::cout << format("  - The ", bbFormatIter->second, " format is not supported") << std::endl;
+                } else {
+                    bbPP.BackBufferFormat = bbFormatIter->first;
+
+                    status = createDeviceWithFlags(&bbPP, D3DCREATE_HARDWARE_VERTEXPROCESSING, false);
+                    if (FAILED(status)) {
+                        std::cout << format("  ! The ", bbFormatIter->second, " format is supported, device creation FAILED") << std::endl;
+                    } else {
+                        std::cout << format("  + The ", bbFormatIter->second, " format is supported, device creation SUCCEEDED") << std::endl;
+                    }
+                }
+            }
+        }
+
+        // D3D Device capabilities check
+        void listDeviceCapabilities() {
+            D3DCAPS9 caps9SWVP;
+            D3DCAPS9 caps9HWVP;
+            D3DCAPS9 caps9;
+
+            // these are all the possible VS versions
+            std::map<DWORD, char const*> vsVersion = { {D3DVS_VERSION(1,1), "1.1"},
+                                                       {D3DVS_VERSION(2,0), "2.0"},
+                                                       {D3DVS_VERSION(3,0), "3.0"} };
+
+            // these are all the possible PS versions
+            std::map<DWORD, char const*> psVersion = { {D3DPS_VERSION(1,1), "1.1"},
+                                                       {D3DPS_VERSION(1,2), "1.2"},
+                                                       {D3DPS_VERSION(1,3), "1.3"},
+                                                       {D3DPS_VERSION(1,4), "1.4"},
+                                                       {D3DPS_VERSION(2,0), "2.0"},
+                                                       {D3DPS_VERSION(3,0), "3.0"} };
+
+            // get the capabilities from the D3D device in SWVP mode
+            createDeviceWithFlags(&m_pp, D3DCREATE_SOFTWARE_VERTEXPROCESSING, true);
+            m_device->GetDeviceCaps(&caps9SWVP);
+
+            // get the capabilities from the D3D device in HWVP mode
+            createDeviceWithFlags(&m_pp, D3DCREATE_HARDWARE_VERTEXPROCESSING, true);
+            m_device->GetDeviceCaps(&caps9HWVP);
+
+            // get the capabilities from the D3D interface
+            m_d3d->GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps9);
+
+            std::cout << std::endl << "Listing device capabilities support:" << std::endl;
+
+            if (caps9.Caps2 & D3DCAPS2_CANAUTOGENMIPMAP)
+                std::cout << "  + D3DCAPS2_CANAUTOGENMIPMAP is supported" << std::endl;
+            else
+                std::cout << "  - D3DCAPS2_CANAUTOGENMIPMAP is not supported" << std::endl;
+
+            if (caps9.Caps3 & D3DCAPS3_LINEAR_TO_SRGB_PRESENTATION)
+                std::cout << "  + D3DCAPS3_LINEAR_TO_SRGB_PRESENTATION is supported" << std::endl;
+            else
+                std::cout << "  - D3DCAPS3_LINEAR_TO_SRGB_PRESENTATION is not supported" << std::endl;
+
+            if (caps9.Caps3 & D3DCAPS3_COPY_TO_VIDMEM)
+                std::cout << "  + D3DCAPS3_COPY_TO_VIDMEM is supported" << std::endl;
+            else
+                std::cout << "  - D3DCAPS3_COPY_TO_VIDMEM is not supported" << std::endl;
+
+            if (caps9.Caps3 & D3DCAPS3_COPY_TO_SYSTEMMEM)
+                std::cout << "  + D3DCAPS3_COPY_TO_SYSTEMMEM is supported" << std::endl;
+            else
+                std::cout << "  - D3DCAPS3_COPY_TO_SYSTEMMEM is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_INDEPENDENTWRITEMASKS)
+                std::cout << "  + D3DPMISCCAPS_INDEPENDENTWRITEMASKS is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_INDEPENDENTWRITEMASKS is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_PERSTAGECONSTANT)
+                std::cout << "  + D3DPMISCCAPS_PERSTAGECONSTANT is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_PERSTAGECONSTANT is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_FOGANDSPECULARALPHA)
+                std::cout << "  + D3DPMISCCAPS_FOGANDSPECULARALPHA is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_FOGANDSPECULARALPHA is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_SEPARATEALPHABLEND)
+                std::cout << "  + D3DPMISCCAPS_SEPARATEALPHABLEND is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_SEPARATEALPHABLEND is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_MRTINDEPENDENTBITDEPTHS)
+                std::cout << "  + D3DPMISCCAPS_MRTINDEPENDENTBITDEPTHS is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_MRTINDEPENDENTBITDEPTHS is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_MRTPOSTPIXELSHADERBLENDING)
+                std::cout << "  + D3DPMISCCAPS_MRTPOSTPIXELSHADERBLENDING is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_MRTPOSTPIXELSHADERBLENDING is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_FOGVERTEXCLAMPED)
+                std::cout << "  + D3DPMISCCAPS_FOGVERTEXCLAMPED is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_FOGVERTEXCLAMPED is not supported" << std::endl;
+
+            if (caps9.PrimitiveMiscCaps & D3DPMISCCAPS_POSTBLENDSRGBCONVERT)
+                std::cout << "  + D3DPMISCCAPS_POSTBLENDSRGBCONVERT is supported" << std::endl;
+            else
+                std::cout << "  - D3DPMISCCAPS_POSTBLENDSRGBCONVERT is not supported" << std::endl;
+
+            if (caps9.RasterCaps & D3DPRASTERCAPS_SCISSORTEST)
+                std::cout << "  + D3DPRASTERCAPS_SCISSORTEST is supported" << std::endl;
+            else
+                std::cout << "  - D3DPRASTERCAPS_SCISSORTEST is not supported" << std::endl;
+
+            if (caps9.RasterCaps & D3DPRASTERCAPS_SLOPESCALEDEPTHBIAS)
+                std::cout << "  + D3DPRASTERCAPS_SLOPESCALEDEPTHBIAS is supported" << std::endl;
+            else
+                std::cout << "  - D3DPRASTERCAPS_SLOPESCALEDEPTHBIAS is not supported" << std::endl;
+
+            if (caps9.RasterCaps & D3DPRASTERCAPS_DEPTHBIAS)
+                std::cout << "  + D3DPRASTERCAPS_DEPTHBIAS is supported" << std::endl;
+            else
+                std::cout << "  - D3DPRASTERCAPS_DEPTHBIAS is not supported" << std::endl;
+
+            if (caps9.RasterCaps & D3DPRASTERCAPS_MULTISAMPLE_TOGGLE)
+                std::cout << "  + D3DPRASTERCAPS_MULTISAMPLE_TOGGLE is supported" << std::endl;
+            else
+                std::cout << "  - D3DPRASTERCAPS_MULTISAMPLE_TOGGLE is not supported" << std::endl;
+
+            if (caps9.SrcBlendCaps & D3DPBLENDCAPS_INVSRCCOLOR2)
+                std::cout << "  + D3DPBLENDCAPS_INVSRCCOLOR2 is supported" << std::endl;
+            else
+                std::cout << "  - D3DPBLENDCAPS_INVSRCCOLOR2 is not supported" << std::endl;
+
+            if (caps9.SrcBlendCaps & D3DPBLENDCAPS_SRCCOLOR2)
+                std::cout << "  + D3DPBLENDCAPS_SRCCOLOR2 is supported" << std::endl;
+            else
+                std::cout << "  - D3DPBLENDCAPS_SRCCOLOR2 is not supported" << std::endl;
+
+            if (caps9.LineCaps & D3DLINECAPS_ANTIALIAS)
+                std::cout << "  + D3DLINECAPS_ANTIALIAS is supported" << std::endl;
+            else
+                std::cout << "  - D3DLINECAPS_ANTIALIAS is not supported" << std::endl;
+
+            if (caps9.StencilCaps & D3DSTENCILCAPS_TWOSIDED)
+                std::cout << "  + D3DSTENCILCAPS_TWOSIDED is supported" << std::endl;
+            else
+                std::cout << "  - D3DSTENCILCAPS_TWOSIDED is not supported" << std::endl;
+
+            std::cout << std::endl << "Listing device capability limits:" << std::endl;
+
+            std::cout << format("  ~ MaxTextureWidth: ", caps9.MaxTextureWidth) << std::endl;
+            std::cout << format("  ~ MaxTextureHeight: ", caps9.MaxTextureHeight) << std::endl;
+            std::cout << format("  ~ MaxVolumeExtent: ", caps9.MaxVolumeExtent) << std::endl;
+            std::cout << format("  ~ MaxTextureRepeat: ", caps9.MaxTextureRepeat) << std::endl;
+            std::cout << format("  ~ MaxTextureAspectRatio: ", caps9.MaxTextureAspectRatio) << std::endl;
+            std::cout << format("  ~ MaxAnisotropy: ", caps9.MaxAnisotropy) << std::endl;
+            std::cout << format("  ~ MaxVertexW: ", caps9.MaxVertexW) << std::endl;
+            std::cout << format("  ~ GuardBandLeft: ", caps9.GuardBandLeft) << std::endl;
+            std::cout << format("  ~ GuardBandTop: ", caps9.GuardBandTop) << std::endl;
+            std::cout << format("  ~ GuardBandRight: ", caps9.GuardBandRight) << std::endl;
+            std::cout << format("  ~ GuardBandBottom: ", caps9.GuardBandBottom) << std::endl;
+            std::cout << format("  ~ ExtentsAdjust: ", caps9.ExtentsAdjust) << std::endl;
+            std::cout << format("  ~ MaxTextureBlendStages: ", caps9.MaxTextureBlendStages) << std::endl;
+            std::cout << format("  ~ MaxSimultaneousTextures: ", caps9.MaxSimultaneousTextures) << std::endl;
+            // may vary between interface and device modes (SWVP or HWVP)
+            std::cout << format("  ~ MaxActiveLights: ", caps9.MaxActiveLights, " (I), ", caps9SWVP.MaxActiveLights,
+                                " (SWVP), ", caps9HWVP.MaxActiveLights, " (HWVP)") << std::endl;
+            // may vary between interface and device modes (SWVP or HWVP)
+            std::cout << format("  ~ MaxUserClipPlanes: ", caps9.MaxUserClipPlanes, " (I), ", caps9SWVP.MaxUserClipPlanes,
+                                " (SWVP), ", caps9HWVP.MaxUserClipPlanes, " (HWVP)") << std::endl;
+            // may vary between interface and device modes (SWVP or HWVP)
+            std::cout << format("  ~ MaxVertexBlendMatrices: ", caps9.MaxVertexBlendMatrices, " (I), ", caps9SWVP.MaxVertexBlendMatrices,
+                                " (SWVP), ", caps9HWVP.MaxVertexBlendMatrices, " (HWVP)") << std::endl;
+            // may vary between interface and device modes (SWVP or HWVP)
+            std::cout << format("  ~ MaxVertexBlendMatrixIndex: ", caps9.MaxVertexBlendMatrixIndex, " (I), ", caps9SWVP.MaxVertexBlendMatrixIndex,
+                                " (SWVP), ", caps9HWVP.MaxVertexBlendMatrixIndex, " (HWVP)") << std::endl;                         
+            std::cout << format("  ~ MaxPointSize: ", caps9.MaxPointSize) << std::endl;
+            std::cout << format("  ~ MaxPrimitiveCount: ", caps9.MaxPrimitiveCount) << std::endl;
+            std::cout << format("  ~ MaxVertexIndex: ", caps9.MaxVertexIndex) << std::endl;
+            std::cout << format("  ~ MaxStreams: ", caps9.MaxStreams) << std::endl;
+            std::cout << format("  ~ MaxStreamStride: ", caps9.MaxStreamStride) << std::endl;
+            std::cout << format("  ~ VertexShaderVersion: ", vsVersion[caps9.VertexShaderVersion]) << std::endl;
+            std::cout << format("  ~ MaxVertexShaderConst: ", caps9.MaxVertexShaderConst) << std::endl;
+            std::cout << format("  ~ PixelShaderVersion: ", psVersion[caps9.PixelShaderVersion]) << std::endl;
+            // typically FLT_MAX
+            std::cout << format("  ~ PixelShader1xMaxValue: ", caps9.PixelShader1xMaxValue) << std::endl;
+            std::cout << format("  ~ MaxNpatchTessellationLevel: ", caps9.MaxNpatchTessellationLevel) << std::endl;
+            std::cout << format("  ~ NumSimultaneousRTs: ", caps9.NumSimultaneousRTs) << std::endl;
+            std::cout << format("  ~ MaxVShaderInstructionsExecuted: ", caps9.MaxVShaderInstructionsExecuted) << std::endl;
+            std::cout << format("  ~ MaxPShaderInstructionsExecuted: ", caps9.MaxPShaderInstructionsExecuted) << std::endl;
+            std::cout << format("  ~ MaxVertexShader30InstructionSlots: ", caps9.MaxVertexShader30InstructionSlots) << std::endl;
+            std::cout << format("  ~ MaxPixelShader30InstructionSlots: ", caps9.MaxPixelShader30InstructionSlots) << std::endl;
+        }
+
+        void listVCacheQueryResult() {
+            resetOrRecreateDevice();
+
+            Com<IDirect3DQuery9> query;
+            D3DDEVINFO_VCACHE vCache;
+
+            HRESULT status = m_device->CreateQuery(D3DQUERYTYPE_VCACHE, &query);
+            if (SUCCEEDED(status)) {
+                status = query->GetData(&vCache, sizeof(D3DDEVINFO_VCACHE), D3DQUERYTYPE_VCACHE);
+            }
+
+            std::cout << std::endl << "Listing VCache query result:" << std::endl;
+
+            switch (status) {
+                // NVIDIA will return D3D_OK and some values in vCache
+                case D3D_OK:
+                    std::cout << "  ~ Response: D3D_OK" << std::endl;
+                    break;
+                // AMD/ATI and Intel will return D3DERR_NOTAVAILABLE
+                case D3DERR_NOTAVAILABLE:
+                    std::cout << "  ~ Response: D3DERR_NOTAVAILABLE" << std::endl;
+                    break;
+                // shouldn't be returned by any vendor
+                case D3DERR_INVALIDCALL:
+                    std::cout << "  ~ Response: D3DERR_INVALIDCALL" << std::endl;
+                    break;
+                default:
+                    std::cout << "  ~ Response: UNKNOWN" << std::endl;
+                    break;
+            }
+
+            if (SUCCEEDED(status)) {
+                char pattern[] = "\0\0\0\0";
+                if (vCache.Pattern == 0) {
+                    pattern[0] = '0';
+                } else {
+                    for (UINT i = 0; i < 4; i++) {
+                        pattern[i] = char(vCache.Pattern >> i * 8);
+                    }
+                }
+
+                std::cout << format("  ~ Pattern: ", pattern) << std::endl;
+                std::cout << format("  ~ OptMethod: ", vCache.OptMethod) << std::endl;
+                std::cout << format("  ~ CacheSize: ", vCache.CacheSize) << std::endl;
+                std::cout << format("  ~ MagicNumber: ", vCache.MagicNumber) << std::endl;
+            }
+        }
+
+        void startTests() {
+            m_totalTests  = 0;
+            m_passedTests = 0;
+
+            std::cout << std::endl << "Running D3D9 tests:" << std::endl;
+        }
+
+        void printTestResults() {
+            std::cout << format("Passed ", m_passedTests, "/", m_totalTests, " tests") << std::endl;
         }
 
         void prepare() {
@@ -210,6 +491,29 @@ class RGBTriangle {
             return status;
         }
 
+        HRESULT resetOrRecreateDevice() {
+            if (m_d3d == nullptr)
+                throw Error("The D3D9 interface hasn't been initialized");
+
+            HRESULT status = D3D_OK;
+
+            // return early if the call to Reset() works
+            if (m_device != nullptr) {
+                if(SUCCEEDED(m_device->Reset(&m_pp)))
+                    return status;
+                // prepare to clear the device otherwise
+                m_device = nullptr;
+            }
+
+            status = m_d3d->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, m_hWnd,
+                                         D3DCREATE_HARDWARE_VERTEXPROCESSING,
+                                         &m_pp, &m_device);
+            if (FAILED(status))
+                throw Error("Failed to create D3D9 device");
+
+            return status;
+        }
+
         HWND                          m_hWnd;
 
         Com<IDirect3D9>               m_d3d;
@@ -217,6 +521,9 @@ class RGBTriangle {
         Com<IDirect3DVertexBuffer9>   m_vb;
 
         D3DPRESENT_PARAMETERS         m_pp;
+
+        UINT                          m_totalTests;
+        UINT                          m_passedTests;
 };
 
 LRESULT WINAPI WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -247,6 +554,14 @@ int main(int, char**) {
 
         // list various D3D Device stats
         rgbTriangle.listAdapterDisplayModes();
+        rgbTriangle.listBackBufferFormats(FALSE);
+        rgbTriangle.listBackBufferFormats(TRUE);
+        rgbTriangle.listDeviceCapabilities();
+        rgbTriangle.listVCacheQueryResult();
+
+        // run D3D Device tests
+        rgbTriangle.startTests();
+        rgbTriangle.printTestResults();
 
         // D3D9 triangle
         rgbTriangle.prepare();
